f3851712e7358a3fd1bd62f7591d0708323cbeae
Filippo Casadei <casadeifilippo91@gmail.com>
2026-05-04 16:19:43 +0200

    -Modificato file App.jsx -Creata la struttura grafica della pagina senza logica con: titolo, form per inserire un nuovo articolo (titolo + testo e pulsante "aggiungi articolo" + sezione con gli articoli pubblicati.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,39 @@
 import './App.css'
 
 function App() {
+    return (
+        <div className="container mt-4">
 
+            <h1 className="mb-4">Blog Articoli</h1>
+
+            <div className="card p-3 mb-4">
+
+                <h4>Inserisci nuovo articolo</h4>
+
+                <div className="mb-3">
+                    <label className="form-label">Titolo</label>
+                    <input className="form-control" />
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label">Contenuto</label>
+                    <textarea className="form-control"></textarea>
+                </div>
+
+                <button className="btn btn-primary">
+                    Aggiungi articolo
+                </button>
+
+            </div>
+
+            <div>
+                <h4>Articoli pubblicati</h4>
+
+                <p>Nessun articolo ancora presente</p>
+            </div>
+
+        </div>
+    );
 };
 
 export default App
